b694dbf47df99668e4bc7c8934c73da9e661ce45
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-11-30 00:01:11 +0530

    nothing

--- middleware.ts
+++ middleware.ts
@@ -2,35 +2,29 @@ import { RedirectToSignIn } from '@clerk/nextjs';
 import { clerkMiddleware, createRouteMatcher, getAuth } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-// Define public route matcher (only / is public)
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
 
 export default clerkMiddleware(async (auth, req) => {
-  // Get user auth details
+  
   const { userId, orgId, redirectToSignIn } = await auth();
 
-  // If the route is public (only "/"), no need for authentication
   if (isPublicRoute(req)) {
-    // If the user is authenticated, redirect to either /select-org or /organization/{orgId}
     if (userId) {
       if (orgId) {
         const redirectToOrg = new URL(`/organization/${orgId}`, req.url);
-        return NextResponse.redirect(redirectToOrg); // Redirect to user's organization page
+        return NextResponse.redirect(redirectToOrg); 
       }
 
-      // If user is authenticated but has no organization, redirect to /select-org
       const redirectToOrgSelection = new URL('/select-org', req.url);
       return NextResponse.redirect(redirectToOrgSelection);
     }
-    return NextResponse.next(); // Public route for unauthenticated users
+    return NextResponse.next(); 
   }
 
-  // If user is not authenticated and trying to access a non-public route
   if (!userId && !isPublicRoute(req)) {
-    return redirectToSignIn(); // Redirect to sign-in page
+    return redirectToSignIn(); 
   }
 
-  // If authenticated but no organization, and the current path is not /select-org, redirect to /select-org
   if (userId && !orgId && req.nextUrl.pathname !== '/select-org') {
     const redirectToOrgSelection = new URL('/select-org', req.url);
     return NextResponse.redirect(redirectToOrgSelection);
