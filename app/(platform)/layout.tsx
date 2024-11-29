41582ad953bbbc960f966c3913bb17e20fd46928
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-11-29 20:59:44 +0530

    authentication done

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,15 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
+const PlatformLayout=({
+    children
+}:{
+    children:React.ReactNode
+})=>{
+    return (
+        <ClerkProvider afterSignOutUrl={"/"}>
+            {children}
+        </ClerkProvider>
+    )
+}
+
+export default PlatformLayout
